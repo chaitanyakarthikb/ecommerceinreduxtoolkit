8a6e7510c4e67cedadec24e2c4c8c662784159a2
Chaitanya karthik basva <chaitanyakarthikbasva@gmail.com>
2025-10-23 22:08:51 +0530

    implemented heroSection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductThunk } from "../Features/ProductsSlice";
+import styled from "styled-components";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -13,10 +15,43 @@ const Home = () => {
     dispatch(getProductThunk());
   }, []);
   return (
-    <div>
-      <h1>THis is my Home Page</h1>
-    </div>
+    <Wrapper>
+      <HeroSection />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  min-height: 450px;
+  padding: 0rem 5rem;
+
+  .hero--section--img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .content--container h1 {
+    margin-top: 0;
+  }
+
+  #content--welcome {
+    font-size: 2rem;
+    color: rgb(81, 56, 238);
+    text-transform: uppercase;
+  }
+
+  .hero--section--img img {
+    width: 75%;
+  }
+
+  .container {
+    margin-top: 15rem;
+  }
+
+  #content--para {
+    margin-bottom: 2rem;
+  }
+`;
 
 export default Home;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+const HeroSection = () => {
+  return (
+    <div className="container">
+      <div className="grid grid-two-column">
+        <div className="content--container">
+          <p id="content--welcome">Welcome to</p>
+          <h1>Basva Store</h1>
+          <p id="content--para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            placeat veritatis voluptates beatae, sapiente impedit nihil corrupti
+            eveniet obcaecati ex possimus non doloribus dicta, excepturi natus
+            necessitatibus sit provident ullam reiciendis soluta. Itaque vero
+            amet quos corrupti nesciunt nam hic.
+          </p>
+          <Button>
+            <Link to={"/products"}>Shop Now</Link>
+          </Button>
+        </div>
+
+        <div className="hero--section--img">
+          <figure>
+            <img src="/images/hero.jpg" alt="Hero" />
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
